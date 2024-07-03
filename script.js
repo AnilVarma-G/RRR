beb93a8e6d42cdf559c23622bf6f33d404c972ae
AnilVarma-G <ristoglobaltech.md@gmail.com>
2024-07-03 23:51:59 +0530

    added

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ function toggleMenu(event) {
 }
 
 
-/**
- * Creator           : Coding's Time
- * Youtube Channel   : https://www.youtube.com/channel/UC6dnKqrImGWqMb9ty1n0Ziw
- * Github Profile    : https://github.com/codingstime
- */
+
 
 let onSlide = false;
 
